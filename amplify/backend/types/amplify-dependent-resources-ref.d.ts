8fadeffc59efba3b104a438167d7b411a406f7b6
Oriol Escrigas Burillo <ooeb@gmv.com>
2022-11-13 11:23:16 +0100

    Adding Auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "autentificacioapp04f3dfaa": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
